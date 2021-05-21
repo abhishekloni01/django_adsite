e1e36763add924774b792650d21d96c1e23d0506
Abhishek Loni <abhishekloni01@gmail.com>
2021-05-22 00:05:28 +0530

    edited

--- adsite/settings.py
+++ adsite/settings.py
@@ -133,8 +133,9 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'assets')
 
 # Activating django heroku by me
 
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
